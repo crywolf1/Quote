bcfc6a4a8310ce91f9559bf534da930105d4f970
unknown <danial.master1996@gmail.com>
2025-03-26 17:47:41 +0330

    farcaster update grok 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
+import { Suspense } from "react";
 import Card from "@/components/Card";
 import CanvasBackground from "../components/CanvasBackground";
-import "../styles/globals.css";
+
 export default function Home() {
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
       <CanvasBackground />
       <div className="relative z-10">
-        <Card />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Card />
+        </Suspense>
       </div>
     </div>
   );
