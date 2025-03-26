045d7d318ca0d3dfa8466c0a8efcfd4c8ffa4760
unknown <danial.master1996@gmail.com>
2025-03-26 17:50:31 +0330

    farcaster update grok 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import Card from "@/components/Card";
 import CanvasBackground from "../components/CanvasBackground";
-
+import "../styles/globals.css";
 export default function Home() {
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
