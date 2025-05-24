caaf49f18e6e24def92482375a63e770a9905646
unknown <danial.master1996@gmail.com>
2025-05-24 19:43:22 +0330

    drop

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
                 "name": "Quote",
                 "url": "https://quote-dusky.vercel.app/",
                 "splashImageUrl": "https://quote-dusky.vercel.app/QuoteIcon.png",
-                "splashBackgroundColor": "#7C65C1"
+                "splashBackgroundColor": "#8F5CE0"
               }
             }
           }'
